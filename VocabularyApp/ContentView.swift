00e3de323bd5b1fb4511e73715339e4ba11520ab
mina.shokry-ext <mina.shokry-ext@ab-inbev.com>
2025-03-30 22:34:58 +0200

    Initial Commit

---
+++ VocabularyApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VocabularyApp
+//
+//  Created by Mina Wefky on 30/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VocabularyApp/VocabularyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocabularyAppApp.swift
+//  VocabularyApp
+//
+//  Created by Mina Wefky on 30/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct VocabularyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
